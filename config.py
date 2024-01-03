c7579b4ee81ae082004fb32e926bd27dc8524e06
arm-out <arminsuraj@gmail.com>
2024-01-02 23:38:25 -0800

    data loader

---
+++ config.py
@@ -0,0 +1,12 @@
+from pathlib import Path
+
+DATA_DIR = Path(__file__).resolve().parent / "frames"
+TRAIN_DIR = DATA_DIR / "train"
+TEST_DIR = DATA_DIR / "test"
+VALID_DIR = DATA_DIR / "val"
+LOG_DIR = Path(__file__).resolve().parent / "log"
+
+REC_LOSS = 0.8 * 255
+PERCEP_LOSS = 0.005
+WRAP_LOSS = 0.4 * 255
+SMOOTH_LOSS = 1.0
